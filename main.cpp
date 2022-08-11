7f4398bd0d5245bf247987af41e7d8364f2af68f
ajp109 <57534418+ajp109@users.noreply.github.com>
2022-08-11 11:56:13 +0100

    Correct code in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include "mbed.h"
 
-int main() {
+DigitalOut green(D2);
+DigitalOut red(D3);
+
+int main()
+{
     while (true) {
-        sleep();
+        green = true;
+        thread_sleep_for(500);
+        green = false;
+        thread_sleep_for(2500);
     }
 }
